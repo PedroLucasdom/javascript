002f19be9da4a8b1e8721562c4dabb2fad947be9
Pedro Lucas de Oliveira Matias <121614798+PedroLucasdom@users.noreply.github.com>
2023-03-02 10:24:18 -0300

    Create ex008.js

---
+++ aula11/ex008.js
@@ -0,0 +1,7 @@
+var vel = 80
+console.log(`A velocidade do seu carro é de ${vel}km/h`)
+if (vel > 80){
+    console.log(`Você ultrapassou o limite de velocidade. MULTADO!`)
+}else{
+    console.log(`Tenha um ótima viagem, dirija sempre com cuidado`)
+}
