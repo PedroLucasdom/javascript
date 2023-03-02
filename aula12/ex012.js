1b01672bfe1bc87d71b553c53d0dfe64ace3ca7c
Pedro Lucas de Oliveira Matias <121614798+PedroLucasdom@users.noreply.github.com>
2023-03-02 15:17:11 -0300

    criei ex012 e ex013

---
+++ aula12/ex012.js
@@ -0,0 +1,11 @@
+var dinheiro = 1800
+
+if (dinheiro >= 5000) {
+    console.log('irruuuu. partiu disney')
+} else if (dinheiro >= 3000) {
+    console.log('Vamos para são paulo')
+} else if (dinheiro >= 2000) {
+    console.log('Vou passear pelo Rio de Janeiro')
+} else {
+    console.log('Vou ficar em casa mesmo')
+}
