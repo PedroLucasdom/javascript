2ba5ef9adc15ced527e9e3d8cf186490a68a0bc4
Pedro Lucas de Oliveira Matias <121614798+PedroLucasdom@users.noreply.github.com>
2023-03-03 12:16:44 -0300

    criei o exercicio hora do dia

---
+++ aula13/script.js
@@ -0,0 +1,16 @@
+var agora = new Date()
+var hora = 21
+var corpo = window.document.body
+var p = window.document.getElementById('p')
+var imagem = window.document.getElementById('imagem')
+
+p.innerHTML = `<p> Agora são ${hora} horas. </p>`
+if (hora >= 5 && hora <= 11) {
+    corpo.style.background = 'khaki'
+}else if (hora >= 12 && hora <= 18) {
+    corpo.style.background = 'chocolate'
+    imagem.innerHTML = '<img src="image/pordosol.jpg" alt="pordosol">'
+}else {
+    corpo.style.background = 'darkslategrey'
+    imagem.innerHTML = '<img src="image/noite.jpg" alt="noite">'
+}
